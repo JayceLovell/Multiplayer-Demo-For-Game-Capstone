e18f03b2169666fa8a67d3e0fc4f6f33a2763e95
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2022-06-09 15:37:45 -0400

    Internet Test Plus Added UI to Game Scene

---
+++ Multiplayer-Demo-For-Game-Capstone/Assets/_Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameController : MonoBehaviour
+{
+    private int _amountOfPlayers;
+    private GameManager gameManager;
+    private NetworkManager networkmanager;
+
+    public bool IsDebugging=true;
+    public int amountOfPlayers { get { return _amountOfPlayers; } set { _amountOfPlayers = value; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        if (IsDebugging)
+        {
+            Debugging();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Ready()
+    {
+
+    }
+    /// <summary>
+    /// method for initilizing debug stuff
+    /// </summary>
+    private void Debugging()
+    {
+        GameObject.Find("TxtStatus").GetComponent<Text>().text = "Status: " + gameManager.Status;
+    }
+}
--- Multiplayer-Demo-For-Game-Capstone/Assets/_Scripts/GameManager.cs
+++ Multiplayer-Demo-For-Game-Capstone/Assets/_Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
 
     private string _hostaddress;
     private string _UserID;
+    private string _status;
     private NetworkManager _networkManager;
 
     public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
@@ -22,6 +23,11 @@ public class GameManager : MonoBehaviour
     {
         get { return _hostaddress; }
         set { _hostaddress = value; } 
+    }
+    public string Status
+    {
+        get { return _status; }
+        set { _status = value; }
     }
 
     /// <summary>
@@ -56,12 +62,14 @@ public class GameManager : MonoBehaviour
     public void HostGame(string _userID)
     {
         UserID = _userID;
+        Status = "Hosting";
         _networkManager.StartHost();
         Debug.Log("Hosting Game");
     }
     public void JoinGame(string _userID)
     {
         UserID = _userID;
+        Status = "Guest";
         if (_hostaddress != null)
         {
             _networkManager.networkAddress = _hostaddress;
