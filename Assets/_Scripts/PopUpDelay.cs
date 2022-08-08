b85d5c65efd32284b54f0b26e2e859b6f3041535
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2022-08-08 16:26:34 -0400

    Added delay and reverb functionality
    #2961809796

---
+++ Assets/_Scripts/PopUpDelay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpDelay: MonoBehaviour
+{
+    public GameObject PopUpPrefab;
+
+    public void Popup(StripValues stripValueScript)
+    {
+        PopUpPrefab.SetActive(true);
+        PopUpPrefab.GetComponent<DelayPopUpScript>().StripValues = stripValueScript;
+    }
+}
